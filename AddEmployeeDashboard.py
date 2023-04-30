df0c6d56b2071060bf00d68fe62396f896deff9e
Ziyad-Aljaser <Ziyad.M.Aljaser@gmail.com>
2023-04-30 01:02:20 -0400

    In the AddEmployeeDashboard class, "Add Employee" button tested and worked + "Go Back" button tested and worked as well.

--- AddEmployeeDashboard.py
+++ AddEmployeeDashboard.py
@@ -1,29 +1,35 @@
-import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QTableWidget, QHeaderView
 
 class AddEmployeeDashboard(QDialog):
 
-    def __init__(self, widget):
+    def __init__(self, widget, func, emp_list):
         super(AddEmployeeDashboard, self).__init__()
         self.widget = widget
+        self.id = 1000
         self.salary = None
         self.position = None
         self.name = None
         loadUi("AddEmployeesGUI.ui", self)
+        self.display_employees_func = func
+        self.emp_list = emp_list
 
         self.EmpButton.clicked.connect(self.new_employee)
 
         self.GoBackButton.clicked.connect(self.switch_dialog)
 
     def new_employee(self):
+        self.id += 1
         self.name = self.EmpNameText.text()
         self.position = self.EmpPositionText.text()
-        self.salary = self.EmpSalaryText.text()
+        self.salary = int(self.EmpSalaryText.text())
+        self.emp_list.append(self.get_employee())
+
+        self.display_employees_func()
 
     def get_employee(self):
-        return {"id": 0000, "name": self.name, "position": self.position,
+        return {"id": self.id, "name": self.name, "position": self.position,
                 "salary": self.salary}
 
     def switch_dialog(self):
--- mainwindow.py
+++ mainwindow.py
@@ -11,9 +11,7 @@ class EmployeesDashboard(QDialog):
         loadUi("EmployeesGUI.ui", self)
 
         self.second_dialog = None
-        self.employees_test = []
-
-        self.load_data()
+        self.employees_list = []
 
         # Used when the "Add Employee" button is clicked, and it opens a new window
         self.addEmployeeButton.clicked.connect(self.go_to_add_new_employee)
@@ -24,46 +22,30 @@ class EmployeesDashboard(QDialog):
     def go_to_add_new_employee(self):
         # Used to ensure the AddEmployeeDashboard class is initialized only once
         if self.second_dialog is None:
-            self.second_dialog = AddEmployeeDashboard(widget)
+            # Passing display_employees function to update the employees list every
+            # time a new employee entered
+            self.second_dialog = AddEmployeeDashboard(widget, self.display_employees, self.employees_list)
             widget.addWidget(self.second_dialog)
         widget.setCurrentIndex(1)
 
-    def load_data(self):
-        # Fake Data
-        self.employees = [
-            {"id": 9000, "name": "EmploeeyZ", "position": "CEO", "salary": 50000},
-            {"id": 1001, "name": "EmploeeyB", "position": "CFO", "salary": 30000},
-            {"id": 1002, "name": "EmploeeyC", "position": "CFO", "salary": 20000},
-            {"id": 1003, "name": "EmploeeyD", "position": "CFO", "salary": 10000},
-            {"id": 1004, "name": "EmploeeyE", "position": "CFO", "salary": 1000},
-            {"id": 1005, "name": "EmploeeyF", "position": "CFO", "salary": 300},
-            {"id": 1006, "name": "EmploeeyG", "position": "CFO", "salary": 3000},
-            {"id": 1007, "name": "EmploeeyH", "position": "CFO", "salary": 400},
-            {"id": 1008, "name": "EmploeeyJ", "position": "CFO", "salary": 6000},
-            {"id": 1009, "name": "EmploeeyK", "position": "CFO", "salary": 7000},
-            {"id": 1010, "name": "EmploeeyL", "position": "CFO", "salary": 9000},
-        ]
-        self.display_employees()
-
     def sort_employees(self, index):
         if index == 0:  # Sort by ID
-            self.employees.sort(key=lambda x: x["id"])
+            self.employees_list.sort(key=lambda x: x["id"])
         elif index == 1:  # Sort by Name
-            print(len(self.employees))
-            self.employees.sort(key=lambda x: x['name'])
-            print(len(self.employees))
+            self.employees_list.sort(key=lambda x: x['name'])
         elif index == 2:  # Sort by Salary: High to Low
-            self.employees.sort(key=lambda x: x['salary'], reverse=True)
+            self.employees_list.sort(key=lambda x: x['salary'], reverse=True)
         elif index == 3:  # Sort by Salary: Low to High
-            self.employees.sort(key=lambda x: x['salary'])
+            self.employees_list.sort(key=lambda x: x['salary'])
 
         # Used to update the displayed employees
         self.display_employees()
 
     def display_employees(self):
+        print("test")
         row = 0
-        self.tableWidget.setRowCount(len(self.employees))
-        for employee in self.employees:
+        self.tableWidget.setRowCount(len(self.employees_list))
+        for employee in self.employees_list:
             self.tableWidget.setItem(row, 0,
                                      QtWidgets.QTableWidgetItem(
                                          str(employee["id"])))
